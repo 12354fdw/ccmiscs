e8cd857f79cba95972d6bd283b4b38cdf12e58bc
12354fdw <88530369+12354fdw@users.noreply.github.com>
2024-07-30 01:17:47 +0700

    Update harder_drive.lua

--- harder_drive.lua
+++ harder_drive.lua
@@ -1,24 +1,31 @@
-local disks = {peripheral.find("drive")}
 peripheral.find("modem",rednet.open)
 
+local original = {}
 local dirs = {}
 
 local spaces = {}
 
 -- init
-for i,d in ipairs(disks) do
-    local dir = d.getMountPath()
-    if dir then
-        print("AVAIL disk: "..dir)
-        table.insert(dirs,dir)
+function getDisks()
+    local disks = {peripheral.find("drive")}
+    for i,d in ipairs(disks) do
+        local dir = d.getMountPath()
+        if dir then
+            print("AVAIL disk: "..dir)
+            table.insert(dirs,dir)
+        end
     end
 end
+
+getDisks()
 
 function getSize()
     for i,v in ipairs(dirs) do
         spaces[v] = fs.getFreeSpace(v)
     end
 end
+
+original = dirs
 
 function write(name,content)
     getSize()
@@ -29,7 +36,7 @@ function write(name,content)
         local space = spaces[v]
         if space ~= 0 then
             local used = space - file_size
-            local write = string.sub(content,get,get+(math.abs(used)-1))
+            local write = string.sub(content,get,get+space-1)
             get = get + space
             local writing = fs.open(v.."/"..name,"w")
             writing.write(write)
@@ -40,6 +47,9 @@ function write(name,content)
                 break
             end
         end
+    end
+    if #wdisks == 0 then
+        error("STORAGE IS FULL (create more storage)")
     end
 end
 
@@ -56,14 +66,104 @@ function read(name)
     return r
 end
 
-local i = 1
-
-while true do
-    while true do
-        write("TESTy"..i, "1234567890")
-        local data = read("TESTy" .. i)
-        
-        i = i + 1
-        sleep(1)
+function rename(name,newname)
+    for i,v in ipairs(dirs) do
+        local dir = v.."/"..name
+        if fs.exists(dir) then
+            fs.move(dir,v.."/"..newname)
+        end
     end
 end
+
+function delete(name)
+    for i,v in ipairs(dirs) do
+        local dir = v.."/"..name
+        if fs.exists(dir) then
+            fs.delete(dir)
+        end
+    end
+end
+
+function healthCheck()
+    while true do
+        term.setTextColor(colors.green)
+        print("HEALTH CHECK")
+        local tfree = 0
+        local tused = 0
+        for i,v in pairs(dirs) do
+            local free = spaces[v] or 0
+            local used = fs.getSize(v) - free
+            tfree = tfree + free
+            tused = tused + used
+            print(v.." - free: "..free..", used: "..used)
+        end
+        print("OVERALL - free: "..tfree..", used: "..tused)
+        term.setTextColor(colors.orange)
+        getDisks()
+        for i,v in pairs(original) do
+            local loss = true
+            for i,pair in pairs(original) do
+                if pair == v then
+                    loss = false
+                end
+            end
+            if loss then
+                print("WARNING: LOST "..v)
+            end
+        end
+        sleep(60)
+    end
+end
+
+function main()
+    while true do
+        local id,msg = rednet.receive("MSS")
+        term.setTextColor(colors.lightGray)
+        getDisks()
+        local type = msg[1]
+        if type then
+    
+            if type == "write" then
+                local name = msg[2]
+                local content = msg[3]
+                print(id.." requested to "..type.." on "..name)
+    
+                if name and content then
+                    write(name,content)
+                end
+            end
+    
+            if type == "read" then
+                local name = msg[2]
+                print(id.." requested to "..type.." "..name)
+    
+                if name then
+                    sleep(0.05)
+                    rednet.send(id,read(name),"MSS")
+                end
+            end
+    
+            if type == "rename" then
+                local name = msg[2]
+                local newname = msg[3]
+                print(id.." requested to "..type.." "..name.." to "..newname)
+    
+                if name and newname then
+                    rename(name,newname)
+                end
+            end
+    
+            if type == "delete" then
+                local name = msg[2]
+                print(id.." requested to "..type.." on "..name)
+    
+                if name then
+                    delete(name)
+                end
+            end
+    
+        end
+    end
+end
+
+parallel.waitForAll(main,healthCheck)
